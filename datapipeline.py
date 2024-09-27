fe63f76bb814292ca07804115f9262e6dc94553b
DragomirBozoki <82651750+DragomirBozoki@users.noreply.github.com>
2024-09-27 10:23:33 +0200

    Add files via upload

--- datapipeline.py
+++ datapipeline.py
@@ -1,6 +1,6 @@
 from preprocessing import *
 
-def data(numspeaker = 1):
+def data(numspeaker):
 
     data = tf.data.Dataset.list_files(f'./data/s{numspeaker}/*.mpg')
     data = data.shuffle(1000, reshuffle_each_iteration=False)
@@ -9,10 +9,7 @@ def data(numspeaker = 1):
     data = data.padded_batch(10, padded_shapes=([75,64,64, 1],[40]))
     data = data.prefetch(tf.data.AUTOTUNE)
 
-    train = data.take(1000)
-    print(train)
-
-    return train
+    return data
 
 def mappable_function(file, numspeaker):
     result = tf.py_function(load_data, [file, numspeaker], (tf.float32, tf.int64))
