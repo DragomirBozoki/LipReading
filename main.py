a8ab2bb0d1543fd659fbd0960a67393ccf2dc11e
DragomirBozoki <82651750+DragomirBozoki@users.noreply.github.com>
2024-09-28 18:07:58 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ from lipreadingnn import *
 import json
 
 def __main__():
+
+    config = tf.compat.v1.ConfigProto()
+    config.intra_op_parallelism_threads = 4
+    config.inter_op_parallelism_threads = 2
+
+    session = tf.compat.v1.Session(config=config)
+    tf.compat.v1.keras.backend.set_session(session)
+
     history = learning()
 
     #sacuvati history u json file
